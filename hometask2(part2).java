6e53da310e6cfc682860aeeeb616a107e6a94c6b
AnnaHutsuliak <59147033+AnnaHutsuliak@users.noreply.github.com>
2020-01-06 00:20:21 +0200

    hometask2(part2).java
    one pound has 453,6 grams

---
+++ hometask2(part2).java
@@ -0,0 +1,13 @@
+
+package com.epam.hometask2;
+public class Hometask2 {
+    public static void main(String[] arg) {
+        double pounds = 120;
+        double grams = 453.6;
+        double result = pounds * grams;
+        System.out.println("result1=" + result/1000);
+        System.out.println("kg=" + 54);
+        System.out.println("g=" + 432);
+    }
+}
+
